b1942786ee9a794a51b527cc205d5226aa049219
Clayvis Rawlins <Clayvis27@gmail.com>
2022-01-30 22:59:32 -0800

    Added Sign in feature and Dark mode based on user's settings

---
+++ src/js/form.js
@@ -0,0 +1,17 @@
+// handles the form submission and the session storage
+
+let form = document
+	.querySelector("#cFormTwo")
+	.addEventListener("submit", handleFormSubmit);
+
+function handleFormSubmit(e) {
+	e.preventDefault();
+	let formEmail = document.getElementById("signInEmail").value;
+	let formPassword = document.getElementById("signPassword").value;
+	//sessionStorage.setItem(formEmail, formPassword);
+	sessionStorage.setItem("status", "isLoggedIn");
+
+	setTimeout(() => {
+		window.location.href = "/index.html";
+	}, 1000);
+}
